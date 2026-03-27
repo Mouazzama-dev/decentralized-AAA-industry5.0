54ea3b266478fd013d74277d855637751e510ebb
Mouazzama-dev <mouazzamabinteumer.ep19102044@gmail.com>
2026-03-27 13:20:39 +0100

    link operator and device

---
+++ src/issue-permit.js
@@ -0,0 +1,34 @@
+import { agent } from '../agent.js'
+
+async function main() {
+  // Database se identities uthayein
+  const operator = await agent.didManagerGetByAlias({ alias: 'operator-marco' })
+  const device = await agent.didManagerGetByAlias({ alias: 'welding-robot-09' })
+
+  console.log(`Issuing permit for ${operator.did} to use ${device.did}...`)
+
+  const vc = await agent.createVerifiableCredential({
+    credential: {
+      "@context": ["https://www.w3.org/ns/credentials/v2"],
+      type: ["VerifiableCredential", "MachineAccessPermit"],
+      issuer: { id: operator.did }, // For now, self-signed (can be changed to Admin later)
+      
+      credentialSubject: {
+        id: operator.did,
+        role: "SeniorWelder",
+        skillLevel: 5,
+        authorizedDeviceId: device.did, // <--- Device link with operator
+        validUntil: "2026-12-31"
+      },
+    },
+    proofFormat: 'jwt',
+  })
+
+  // DB mein save karein
+  await agent.dataStoreSaveVerifiableCredential({ verifiableCredential: vc })
+
+  console.log('\n Industrial Permit Created & Saved!')
+  console.log(JSON.stringify(vc, null, 2))
+}
+
+main().catch(console.log)
